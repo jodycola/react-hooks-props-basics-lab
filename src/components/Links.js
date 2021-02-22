df05cd7edf695f8d37e6b6270889df748b81aed6
Kody Samaroo <github email address>
2021-02-22 16:11:32 -0500

    Done.

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+function Links(props) {
+    return (
+      <div>
+        <h3>Links</h3>
+      <a href={props.github}>GitHub</a>
+      <a href={props.linkedin}>LinkedIn</a>
+      </div>
+    );
+  }
+  
+
+export default Links
